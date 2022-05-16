eef4c0638164dfa4d169cec516e09b9ccd689d2c
还没起名字队 <879410993@qq.com>
2022-05-16 21:02:07 +0800

    feature: add lsm_tree test

--- src/kv/lsm_tree/mod.rs
+++ src/kv/lsm_tree/mod.rs
@@ -4,3 +4,25 @@ pub mod memtable;
 pub mod block_iterator;
 pub mod file_iterator;
 pub mod data_type;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        let mut kv = kv::LSMTree::new();
+        kv.put("a", "b");
+        kv.put("ssa", "ada");
+        assert_eq!(kv.get("a").unwrap(), "b");
+        assert_eq!(kv.get("ssa").unwrap(), "ada");
+        kv.put("a", "bc");
+        assert_eq!(kv.get("a").unwrap(), "bc");
+        for i in 0..100 {
+            kv.put(&i.to_string(), "test");
+        }
+        for i in 0..100 {
+            assert_eq!(kv.get(&i.to_string()).unwrap(), "test");
+        }
+    }
+}
